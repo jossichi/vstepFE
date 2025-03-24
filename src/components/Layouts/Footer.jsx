d900a29bb72c36bab832238f551823cb19a08fb7
jossichi <bloc907@gmail.com>
2025-03-24 23:15:14 +0700

    sua loi footer

--- src/components/Layouts/Footer.jsx
+++ src/components/Layouts/Footer.jsx
@@ -1,28 +1,11 @@
 // Footer.jsx
-import React from 'react';
-import '../../assets/Footer.css';
+import React from "react";
+import "../../assets/Footer.css";
 
 const Footer = () => {
   return (
     <div className="footer-simple">
-      <div className="social-icons">
-        <a href="#"><ion-icon name="logo-instagram"></ion-icon></a>
-        <a href="#"><ion-icon name="logo-snapchat"></ion-icon></a>
-        <a href="#"><ion-icon name="logo-twitter"></ion-icon></a>
-        <a href="#"><ion-icon name="logo-facebook"></ion-icon></a>
-      </div>
-      
-      <div className="footer-links">
-        <a href="#">Home</a>
-        <a href="#">Services</a>
-        <a href="#">About</a>
-        <a href="#">Terms</a>
-        <a href="#">Privacy Policy</a>
-      </div>
-      
-      <div className="footer-copyright">
-        Company Name © 2025
-      </div>
+      <div className="footer-copyright">Company Name © 2025</div>
     </div>
   );
 };
